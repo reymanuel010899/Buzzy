05342a93f8210c29176d1db4dfe9603e7dd3f0be
reymanuel010899 <reymanuelferrera78@gmail.com>
2025-02-20 00:54:17 -0400

    agregando algunas cositas

--- Buzzy/urls.py
+++ Buzzy/urls.py
@@ -1,8 +1,16 @@
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
+from django.conf import settings
+from django.conf.urls.static import serve
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('apps.users.urls', namespace='users')),
     path('media/', include('apps.videos.urls', namespace='video'))
+]
+
+urlpatterns += [
+    re_path(r'^media/(?P<path>.*)$', serve, {
+        'document_root': settings.MEDIA_ROOT,
+    }),
 ]
--- apps/videos/serializers.py
+++ apps/videos/serializers.py
@@ -1,7 +1,27 @@
 from rest_framework import serializers
 from .models import Video
+from apps.users.models import User
+class UserSerializers(serializers.ModelSerializer):
+     class Meta:
+        model = User
+        fields = ("username", "email", 'profile_picture') 
 
 class VideoZerializer(serializers.ModelSerializer):
+    user_id = UserSerializers()
     class Meta:
         model = Video
-        fields = "__all__"  
+        fields = [
+            "id",
+            "user_id",
+            "category",
+            "video_url",
+            "video",
+            "thumbnail_url",
+            "description",
+            "tags",
+            "created_at",
+            "updated_at",
+            "duration",
+            "likes_count",
+            "comments_count",
+        ]
